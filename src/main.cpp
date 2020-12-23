0a47f09ddfd9ecc955c8691b8ee3f7300bb83e70
mahardika denie <dikamahar884@gmail.com>
2020-12-23 11:17:30 +0700

    Perbaikan

--- src/main.cpp
+++ src/main.cpp
@@ -5,59 +5,68 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "\nPola Ke 1\n"<< endl;
-
-    for (int i = 0; i < 6; i++)
-    {
-        for (int v = 0; v < 6; v++)
+    int Input;
+    MainMenu:
+    system("cls");
+    cout<<"Masukan Jumlah Perulangan : ";cin>>Input;
+    if(Input > 3){
+        cout << "\nPola Ke 1\n"<< endl;
+        for (int i = 0; i < 6; i++)
         {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    cout << "\nPola 2\n"<< endl;
-
-    for (int i = 1; i <= 6; i++)
-    {
-        for (int v = 1; v <= 6; v++)
-        {
-            if (i == 1 || i == 6 || v == 1 || v == 6)
+            for (int v = 0; v < 6; v++)
             {
                 cout << "*";
             }
-            else
+            cout << endl;
+        }
+
+        cout << "\nPola 2\n"<< endl;
+
+        for (int i = 1; i <= 6; i++)
+        {
+            for (int v = 1; v <= 6; v++)
+            {
+                if (i == 1 || i == 6 || v == 1 || v == 6)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+
+        cout << "\nPola 3\n"<< endl;
+        for (int i = 1; i <= 6; i++)
+        {
+
+            for (int b = 1; b <= i; b++)
+            {
+
+                cout << "*";
+            }
+            cout << endl;
+        }
+
+        cout << "\nPola 4\n"<< endl;
+
+        for (int i = 1; i <= 6; i++)
+        {
+            for (int z = 1; z < i; z++)
             {
                 cout << " ";
             }
+            for (int o = 6; o >= i; o--)
+            {
+                cout << "*";
+            }
+            cout << endl;
         }
-        cout << endl;
+    }else{
+        goto MainMenu;
     }
-
-    cout << "\nPola 3\n"<< endl;
-    for (int i = 1; i <= 6; i++)
-    {
-
-        for (int b = 1; b <= i; b++)
-        {
-
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    cout << "\nPola 4\n"<< endl;
-
-    for (int i = 1; i <= 6; i++)
-    {
-        for (int z = 1; z < i; z++)
-        {
-            cout << " ";
-        }
-        for (int o = 6; o >= i; o--)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    
+   
 }
